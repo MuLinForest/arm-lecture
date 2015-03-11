ab795e22b8d61800a081de7e94383c33f546dfea
MuLin <Millenary.soul@gmail.com>
2015-03-12 01:51:59 +0800

    Revise the file .gitignore and the file fib.s in the lab-1 directory
    1.rivise the file .gitignore to avoid automatically adding the executable file when committing.
    2.rivese the file fib.s to realize the Fibonacii number function. And it can works in the sample input.

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -14,19 +14,32 @@ fibonacci:
 	push {r3, r4, r5, lr}
 
 	@ R4 = R0 - 0 (update flags)
+	subs R4, R0, #0
+
 	@ if(R0 <= 0) goto .L3 (which returns 0)
+	cmp R4, #1
+	blt .L3
 
 	@ Compare R4 wtih 1
 	@ If R4 == 1 goto .L4 (which returns 1)
+	beq .L4
 
 	@ R0 = R4 - 1
+	sub R0, R4, #1
 	@ Recursive call to fibonacci with R4 - 1 as parameter
+	bl fibonacci
 
 	@ R5 = R0
+	mov R5, R0
+
 	@ R0 = R4 - 2
+	sub R0, R4, #2
+
 	@ Recursive call to fibonacci with R4 - 2 as parameter
+	bl fibonacci
 
 	@ R0 = R5 + R0 (update flags)
+	add R0, R5, R0
 
 	pop {r3, r4, r5, pc}		@EPILOG
 
