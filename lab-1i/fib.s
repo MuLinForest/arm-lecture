7941ecfd8422a246207e1935ce549c84a64e2956
MuLin <Millenary.soul@gmail.com>
2015-03-12 23:38:51 +0800

    Revise the file lab-1i/fib.s to complete Fibonacci number function with non-recursive way.
    Use for-loop to compute the fianl number step by step, which begins with number 0 and number 1.
    Assign register R0 as output value when single loop finishs every time.
    Assign register R3 as the second-to-laste output value.
    Assign register R4 as the number of repetitions.
    Assign register R5 as the temporary value.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -14,19 +14,27 @@ fibonacci:
 	push {r3, r4, r5, lr}
 
 	@ R4 = R0 - 0 (update flags)
+	subs R4, R0, #0
+
 	@ if(R0 <= 0) goto .L3 (which returns 0)
+	cmp R4, #1
+	blt .L3
 
 	@ Compare R4 wtih 1
 	@ If R4 == 1 goto .L4 (which returns 1)
+	beq .L4
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
 
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
+	mov R3, #0
+	mov R0, #1
+	sub R4, #2
+for:
+	mov R5, R0
+	adds R0, R3, R0
+	mov R3, R5
+	subs R4, #1
+	cmp R4, #0
+	bge for
 
 	pop {r3, r4, r5, pc}		@EPILOG
 
